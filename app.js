7dd9c29667c77a444e5830287cc9666ef6259f95
god0g <71331859+god0g@users.noreply.github.com>
2020-10-28 15:54:21 +0700

    add express and templete

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var ejs = require('ejs');
 
 var app = express();
+app.set('view engine','ejs');
+
+app.use(express.static('public'))
 
 app.get('/',function(req,res){
-    res.send('Hello This is first start');
+    res.render('index',{foo:'execute from foo'});
+});
+
+app.get('/form',function(req,res){
+    res.render('form',{foo:'execute form'});
 });
 
 app.listen(3000,()=>{
